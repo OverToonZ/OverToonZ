3090bdaf1e53658ee24033ade4574600f18a10bf
OverToonZ <overtoonz@gmail.com>
2024-11-16 11:18:08 +0530

    Update settings.py

--- overtoonz/settings.py
+++ overtoonz/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-u6tcpui#(#mkkkr00u^evxzeq!0%olf#$+b7jy$=0j7=&a3vxj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['overtoonz.onrender.com']
 
