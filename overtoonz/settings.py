81e24c34e0ec6ca4528056f6acdb000f64d3ba05
OverToonZ <overtoonz@gmail.com>
2024-11-16 10:56:19 +0530

    Update static

--- overtoonz/settings.py
+++ overtoonz/settings.py
@@ -129,9 +129,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Ensure this matches your collectstatic target
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),  # Add this if you have custom static files in your project
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
